839140f0a50f5becafaba82d0b5c24a046aac2f6
aluno <aluno@ifpe>
2024-03-21 16:06:37 -0300

    atualização aula 21/03

--- src/views/cliente/FormCliente.jsx
+++ src/views/cliente/FormCliente.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import InputMask from 'react-input-mask';
+import { Link } from "react-router-dom";
 import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
 export default function FormCliente () {
@@ -88,7 +89,7 @@ export default function FormCliente () {
                                 color='orange'
                             >
                                 <Icon name='reply' />
-                                Voltar
+                                <Link to={'/list-cliente'}>Voltar</Link>
                             </Button>
                                 
                             <Button
